b178a292ddb23578789f374694d18a384a69b78c
a.francisco <afarthuraf@gmail.com>
2021-11-26 12:58:40 +0100

    plus propore

--- public_html/ajax_call.php
+++ public_html/ajax_call.php
@@ -1,15 +1,16 @@
 <?php
-  $request = (isset($_POST['request'])) ? $_POST['request'] : '';
+  CONST DATABASE_URL = "mysql:host=mysql;dbname=vin";
+  CONST USER = "dev";
+  CONST PASSORD = "dev";
+  if (isset($_POST['request'])) {
+    $request = $_POST['request'];
 
-  $dsn = "mysql:host=mysql;dbname=vin";
-  $user = "dev";
-  $passwd = "dev";
+    $pdo = new PDO(DATABASE_URL, USER, PASSORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
-  $pdo = new PDO($dsn, $user, $passwd);
-  $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+    $query = $pdo->prepare("INSERT INTO vin.ChatRequest (firstRequest) VALUES (:firstRequest)");
+    $query->bindParam(':firstRequest', $request);
 
-  $query = $pdo->prepare("INSERT INTO vin.ChatRequest (firstRequest) VALUES (:firstRequest)");
-  $query->bindParam(':firstRequest', $request);
+    $query->execute();
 
-  $query->execute();
-
+  }
